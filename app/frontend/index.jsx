11d9274525db82a9f8c7798b5f1e1217030573b2
Fedorov Anton <fedoroffanton96@gmail.com>
2017-03-10 11:35:42 +0300

    Add template and params on rendering

--- app/frontend/index.jsx
+++ app/frontend/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory, useRouterHistory } from 'react-router'
+import { createHashHistory } from "history"
 import Test from "./modules/test"
 
 class App extends React.Component {
@@ -17,14 +18,19 @@ class App extends React.Component {
   }
 }
 
-VK.init(() => {
-  render((
-    <Router history={hashHistory}>
-      <Route path="/" component={App} />
-      <Route path="/tests/:test_id" component={Test} />
-    </Router>
-  ), document.getElementById('app'))
+let history = useRouterHistory(createHashHistory)()
+if (Hash != '') {
+  history.push(Hash)
+}
+render((
+  <Router history={history}>
+    <Route path="/" component={App} />
+    <Route path="/tests/:test_id" component={Test} />
+  </Router>
+), document.getElementById('app'))
 
+VK.init(() => {
+  console.log('vk init success')
 }, () => {
   console.log("vk init doesn't work!")
 }, '5.62')
--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const bodyParser = require('body-parser')
 const NODE_ENV = process.env.NODE_ENV || 'development'
 const Settings = require('./settings.json')[NODE_ENV]
 const controllers = require('./app/controllers')
+const UserModel = require('./app/models').UserModel
 
 
 const app = express()
+app.set('view engine', 'pug')
+app.set('views', './app/views')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
@@ -16,8 +19,35 @@ if (NODE_ENV !== 'production') {
 }
 
 app.get('/', (req, res) => {
-  console.log('GET /', req)
-  res.sendFile(path.join(__dirname, './app/views/index.html'))
+  if (!req.query.viewer_id) {
+    return res.status(500).send('Error')
+  }
+  let payload = {
+    viewer_id: req.query.viewer_id,
+    hash: req.query.hash || ''
+  }
+  UserModel
+    .findOne({viewer_id: req.query.viewer_id})
+    .then(user => {
+      if (user != null) {
+        console.log('user finded', user)
+        payload.user = user
+        res.render('index', payload)
+      } else {
+        let user = new UserModel({
+          viewer_id: req.query.viewer_id
+        })
+        user
+          .save()
+          .then(user => {
+            console.log('user created', user)
+            payload.user = user
+            res.render('index', payload)
+          })
+          .catch(err => res.status(500).send({error: {message: err.message}}))
+      }
+    })
+    .catch(err => res.status(500).send({error: {message: err.message}}))
 })
 
 app.listen(Settings.port, () => console.log(`listening on port ${Settings.port}`))
