3faea920643949b7303182dd94376127c38f7eb8
Fedorov Anton <fedoroffanton96@gmail.com>
2017-03-16 12:20:16 +0300

    Fix setting

--- app/models/index.js
+++ app/models/index.js
@@ -28,8 +28,6 @@ let AnswerModel        = mongoose.model('Answer', Answer)
 let TestResultModel    = mongoose.model('TestResult', TestResult)
 let RubricModel        = mongoose.model('Rubric', Rubric)
 
-UserModel.remove({})
-AnswerModel.remove({})
 
 module.exports.UserModel          = UserModel
 module.exports.TestModel          = TestModel
